b330902112e0d489669240f3058d6dfd8ed12f2d
Nicolas Froidure <nicolas_froidure@elitwork.com>
2014-01-04 17:47:34 +0100

    Adding streams support, using gulp-util to wrap errors

--- test/compile-with-includes.js
+++ test/compile-with-includes.js
@@ -4,21 +4,22 @@ var lessc = require('less');
 var fs = require('fs');
 var path = require('path');
 var gutil = require('gulp-util');
+var es = require('event-stream');
 
 var expected = fs.readFileSync(path.join(__dirname, 'fixtures', 'test.css'), 'utf8');
 var filePath = path.join(__dirname, 'fixtures', 'test-with-include.less');
 var base = path.join(__dirname, 'fixtures');
 var cwd = __dirname;
-var file = new gutil.File({
-  cwd: cwd,
-  base: base,
-  path: filePath,
-  contents: fs.readFileSync(filePath)
-});
 
 describe('gulp-less', function () {
   describe('less()', function () {
-    it('should compile a less file', function (done) {
+    it('should compile a less file in buffer mode', function (done) {
+      var file = new gutil.File({
+        cwd: cwd,
+        base: base,
+        path: filePath,
+        contents: fs.readFileSync(filePath)
+      });
       var stream = less({
         paths: [ path.join(__dirname, 'fixtures', 'includes') ]
       });
@@ -33,6 +34,32 @@ describe('gulp-less', function () {
         newFile.relative.should.equal('test-with-include.css');
         String(newFile.contents).should.equal(expected);
         done();
+      });
+      stream.write(file);
+    });
+    it('should compile a less file in stream mode', function (done) {
+      var file = new gutil.File({
+        cwd: cwd,
+        base: base,
+        path: filePath,
+        contents: fs.createReadStream(filePath)
+      });
+      var stream = less({
+        paths: [ path.join(__dirname, 'fixtures', 'includes') ]
+      });
+      stream.on('error', done);
+      stream.on('data', function(newFile) {
+        should.exist(newFile);
+        should.exist(newFile.path);
+        should.exist(newFile.relative);
+        should.exist(newFile.contents);
+
+        newFile.path.should.equal(path.join(__dirname, 'fixtures', 'test-with-include.css'));
+        newFile.relative.should.equal('test-with-include.css');
+        file.contents.pipe(es.wait(function(err, data) {
+          data.should.equal(expected);
+          done();
+        }));
       });
       stream.write(file);
     });
