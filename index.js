11af5a65df26d8a6cd6ba366a32ca2fe8b742474
Steve Lacy <me@slacy.me>
2014-07-28 13:14:46 -0700

    stream return style

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+var path = require('path');
 var less = require('less');
 var through2 = require('through2');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
-var path = require('path');
 var defaults = require('lodash.defaults');
 var convert = require('convert-source-map');
 var applySourceMap = require('vinyl-sourcemaps-apply');
@@ -14,17 +14,14 @@ module.exports = function (options) {
     paths: []
   });
 
-  function transform (file, enc, next) {
-    var self = this;
+  return through2.obj(function(file, enc, cb) {
 
     if (file.isNull()) {
-      this.push(file); // pass along
-      return next();
+      return cb(null, file);
     }
 
     if (file.isStream()) {
-      this.emit('error', new PluginError('gulp-less', 'Streaming not supported'));
-      return next();
+      return cb(new PluginError('gulp-less', 'Streaming not supported'));
     }
 
     var str = file.contents.toString('utf8');
@@ -45,18 +42,18 @@ module.exports = function (options) {
     less.render(str, opts, function (err, css) {
       if (err) {
 
-        // convert the keys so PluginError can read them
+        // Convert the keys so PluginError can read them
         err.lineNumber = err.line;
         err.fileName = err.filename;
 
-        // add a better error message
+        // Add a better error message
         err.message = err.message + ' in file ' + err.fileName + ' line no. ' + err.lineNumber;
 
-        self.emit('error', new PluginError('gulp-less', err));
+        return cb(new PluginError('gulp-less', err));
       } else {
         file.contents = new Buffer(css);
         file.path = gutil.replaceExtension(file.path, '.css');
-    
+
         if (file.sourceMap) {
           var comment = convert.fromSource(css);
           if (comment) {
@@ -69,14 +66,10 @@ module.exports = function (options) {
           }
         }
 
-
-        self.push(file);
+        cb(null, file);
       }
-      next();
     });
-  }
-
-  return through2.obj(transform);
+  });
 };
 
 
